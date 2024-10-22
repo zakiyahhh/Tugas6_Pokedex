09c9132acdebf4ba2397fd79917bc51b967fa420
sitizakiyah <sitizakiyah235@gmail.com>
2024-10-23 05:57:50 +0700

    generate data list pokemon dari publik API

--- backend/generateJsonDB.js
+++ backend/generateJsonDB.js
@@ -1,11 +1,18 @@
 const fs = require("fs");
 
 async function generateJsonDB() {
-  // TODO: fetch data pokemon api dan buatlah JSON data sesuai dengan requirement.
-  // json file bernama db.json. pastikan ketika kalian menjalankan npm run start
-  // dan ketika akses url http://localhost:3000/pokemon akan muncul seluruh data
-  // pokemon yang telah kalian parsing dari public api pokemon
-  const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/";
+  try {
+    // TODO: fetch data pokemon api dan buatlah JSON data sesuai dengan requirement.
+    // json file bernama db.json. pastikan ketika kalian menjalankan npm run start
+    // dan ketika akses url http://localhost:3000/pokemon akan muncul seluruh data
+    // pokemon yang telah kalian parsing dari public api pokemon
+
+    const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/?limit=100";
+    const pokemonList = await fetch(pokemonApiURL).then((res) => res.json());
+    console.log(pokemonList);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 generateJsonDB();
